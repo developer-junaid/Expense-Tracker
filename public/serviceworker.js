b8a00d3e96954a5a6de3e7ff1688671e806e7eb5
Junaid <qjunaid623@gmail.com>
2020-11-04 01:55:32 +0500

    Deleted offline file and unnecessary console log

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -7,7 +7,6 @@ const self = this
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened Cache")
       return cache.addAll(filesToCache)
     })
   )
