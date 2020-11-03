9d02a71c76aa260ab27ac758f2658755410a608d
Junaid <qjunaid623@gmail.com>
2020-11-04 02:27:46 +0500

    ALL TESTS PASSED (STABLE CODE)

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,26 +1,44 @@
 const CACHE_NAME = "expenseTrackerCache"
 
-const filesToCache = ["manifest.json", "index.html", "offline.html"]
+const filesToCache = [
+  "/",
+  "manifest.json",
+  "index.html",
+  "/static/js/0.chunk.js",
+  "/static/js/bundle.js",
+  "/static/js/main.chunk.js",
+  "/images/icon-192x192.png",
+  "/images/coins.svg",
+  "/static/media/coins.e505caba.svg",
+  "https://fonts.googleapis.com/css2?family=Acme&display=swap",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap",
+  "https://fonts.gstatic.com/s/acme/v11/RrQfboBx-C5_XxrBbk_m3Lg.woff2",
+
+  // "https://fonts.googleapis.com/css2?family=Acme&display=swap",
+]
 
 const self = this
 // Install SW
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(filesToCache)
+      cache.addAll(filesToCache)
     })
   )
 })
 
 // Listen for requests
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(() =>
-        fetch(event.request).catch(() => caches.match("offline.html"))
-      )
-  )
+  if (!navigator.onLine) {
+    event.respondWith(
+      caches.match(event.request).then((resp) => {
+        if (resp) {
+          return resp
+        }
+      })
+    )
+  }
 })
 
 // Activate the SW
