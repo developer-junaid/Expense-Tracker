fd6b97c20a87e68c52d1c44a1177b6ae9dc76edd
Junaid <qjunaid623@gmail.com>
2020-11-05 00:34:27 +0500

    Added Some Layouts abd Stylings

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,13 @@ import "./../styles/css/balance.css"
 const Balance = () => {
   return (
     <div className="balance-container">
-      <h1>I am balance</h1>
+      <div className="balance">
+        <h3>Current Balance : $1000</h3>
+      </div>
+      <div className="income-expense-container">
+        <h4>Income <br />  $200</h4>
+        <h4>Expense <br /> $400</h4>
+      </div>
     </div>
   )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,39 @@ import "./../styles/css/input.css"
 const Input = () => {
   return (
     <div className="input-container">
-      <h1>I am input</h1>
+      {/* Add Transaction */}
+      <section>
+        <h3>Add New Transaction</h3>
+        <hr />
+
+        {/* FORM */}
+        <form className="transaction-form">
+          <label>
+            <span className="label">Text</span>
+            <br />
+            <input required type="text" placeholder="Enter text..." />
+          </label>
+
+          <br />
+          <br />
+
+          <label>
+            <span className="label">Amount</span>
+            <br />
+            <input required type="number" placeholder="Enter amount..." />
+          </label>
+
+          <br />
+          <div className="buttons-section">
+            <button className="submit-btn" type="submit">
+              Add Income
+            </button>
+            <button className="submit-btn" type="submit">
+              Add Expense
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
   )
 }
