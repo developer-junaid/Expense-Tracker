6b86b30b9ae2d22f2e0d24715e04cfc43f54e8ee
Junaid <qjunaid623@gmail.com>
2020-11-07 17:23:35 +0500

    Token Prompting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react"
 import Main from "./container/Main"
-// import firebase from "./firebase"
+import firebase from "./firebase"
 
 function App() {
-  // // Firebase Messaging
-  // const messaging = firebase.messaging()
-  // messaging
-  //   .requestPermission()
-  //   .then(() => messaging.getToken())
-  //   .then((token) => {
-  //     // prompt("token", token)
-  //   })
+  // Firebase Messaging
+  const messaging = firebase.messaging()
+  messaging
+    .requestPermission()
+    .then(() => messaging.getToken())
+    .then((token) => {
+      prompt("token", token)
+    })
 
   return <Main />
 }
