bd70c9a3f061118a9fa3b277d14ad8ada674f4a8
Junaid <qjunaid623@gmail.com>
2020-11-11 23:47:07 +0500

    Stabilized again !

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -4,21 +4,38 @@ const filesToCache = [
   "/",
   "index.html",
   "manifest.json",
+
   "/static/js/0.chunk.js",
   "/static/js/1.chunk.js",
   "/static/js/bundle.js",
   "/static/js/main.chunk.js",
-  "/static/css/main.5bf085b9.chunk.css",
+  "/static/js/2.chunk.js",
   "/static/js/2.d8dcbfdf.chunk.js",
-  "/static/js/main.5a9a5228.chunk.js",
   "/static/js/3.bcbe146d.chunk.js",
+  "/static/js/main.5a9a5228.chunk.js",
+
+  "/static/css/main.5bf085b9.chunk.css",
+  "/static/css/main.f762c360.chunk.css",
   "/static/css/3.2874e8ad.chunk.css",
-  "/static/media/coins.e505caba.svg",
   "/web-fonts/css/all.min.css",
-  "/images/icon-192x192.png",
+
+  "/static/media/SourceSansPro-Regular.c1678b46.ttf",
+
   "/images/coins.svg",
-  "https://pro.fontawesome.com/releases/v5.10.0/webfonts/fa-solid-900.woff2",
-  "https://pro.fontawesome.com/releases/v5.10.0/webfonts/fa-brands-400.woff2",
+  "/static/media/coins.e505caba.svg",
+
+  "/images/icon-192x192.png",
+
+  "/web-fonts/webfonts/fa-solid-900.woff2",
+  "/web-fonts/webfonts/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7l.woff2",
+  "/web-fonts/webfonts/fa-brands-400.woff2",
+  "/web-fonts/web-fonts/webfonts/fa-brands-400.ttf",
+  "/web-fonts/web-fonts/webfonts/fa-solid-900.ttf",
+  "/web-fonts/web-fonts/webfonts/fa-brands-400.woff",
+  "/web-fonts/web-fonts/webfonts/fa-solid-900.woff",
+  "/web-fonts/web-fonts/webfonts/fa-brands-400.woff2",
+  "/src/styles/fonts/SourceSansPro-Regular.ttf",
+  "/web-fonts/web-fonts/webfonts/fa-solid-900.woff2",
 ]
 
 // Install SW
